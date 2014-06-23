aa9470dd690cd1c5456553f0a079f0f99ad67429
outlawchief <msinko@u.rochester.edu>
2014-06-23 11:50:07 -0400

    Getting it up

---
+++ PythonLeap/arduinoBlink.py
@@ -0,0 +1,27 @@
+# simpleSerialSend.py
+import sys
+import serial
+import time
+PORT = 'COM7' # The port my Arduino is on, on my WinXP box.
+
+def main(val=5):
+    # Open a connection to the serial port.  This will reset the Arduino, and
+    # make the LED flash once:
+    ser = serial.Serial(PORT)
+
+    # Must given Arduino time to rest.
+    # Any time less than this does not seem to work...
+    time.sleep(1.5)
+
+    # Now we can start sending data to it:
+    written = ser.write(val)
+    ser.close()
+    print "Bytes Written to port:", written
+    print "Value written to port: '%s'"%val
+
+if __name__ == '__main__':
+    args = sys.argv
+    try:
+        main(args[1])
+    except IndexError:
+        main()
