b185d72bc05092d3aa7ce318ebc7ad1a4aedfe5b
taylanksen <tsen@cs.rochester.edu>
2014-07-07 09:11:15 -0400

    Added mindmap of uses to documentation.

---
+++ doc/uses.mm
@@ -0,0 +1,134 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1403619015369" ID="ID_546225649" MODIFIED="1403620212003" TEXT="MIDAS Touch Uses">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1403619708117" ID="ID_1943680834" MODIFIED="1403620211786" POSITION="right" TEXT="Entertainment">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1403619992011" ID="ID_1896267357" MODIFIED="1403620211806" TEXT="virtual reality">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1403620000898" ID="ID_937125077" MODIFIED="1403620211808" TEXT="games">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1403620003997" ID="ID_276787143" MODIFIED="1403620211820" TEXT="social communication">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1403620011065" ID="ID_1408403409" MODIFIED="1403620211833" TEXT="hand shake"/>
+<node COLOR="#111111" CREATED="1403620014544" ID="ID_167157620" MODIFIED="1403620211835" TEXT="hold hand"/>
+</node>
+<node COLOR="#990000" CREATED="1403626802746" ID="ID_645835271" MODIFIED="1403626816456" TEXT="skype vibrtion for smile">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1403619773801" ID="ID_1076390701" MODIFIED="1403620211854" POSITION="right" TEXT="Vehicle Operation">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1403620043413" ID="ID_703052230" MODIFIED="1403620211869" TEXT="flight stick">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1403620048422" ID="ID_404633445" MODIFIED="1403620211872" TEXT="in flight refueling">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1403620066106" ID="ID_1659281726" MODIFIED="1403620211879" TEXT="spacecraft docking">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1403619747021" ID="ID_1523632613" MODIFIED="1403620211892" POSITION="left" TEXT="Engineering">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1403619796777" ID="ID_24917210" MODIFIED="1403620211908" TEXT="3D drafting/modeling">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1403619807657" ID="ID_1875589312" MODIFIED="1403620211915" TEXT="telehaptics">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1403619823666" ID="ID_852873521" MODIFIED="1403620211917" TEXT="telerobotics">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1403619829055" ID="ID_1541863459" MODIFIED="1403620211922" TEXT="telemedicine">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1403619750771" ID="ID_1622871899" MODIFIED="1403620211934" POSITION="left" TEXT="Assistive Technology">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1403619851984" ID="ID_963045385" MODIFIED="1403620211940" TEXT="vision impaired/blind">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1403627296191" ID="ID_1373852501" MODIFIED="1403627303848" TEXT="music recognition for the deaf">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1403619886419" ID="ID_1800452601" MODIFIED="1403620211946" TEXT="ability to see 2D/3D for">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1403619923968" ID="ID_660409293" MODIFIED="1403620211948" TEXT="drawing">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1403619928581" ID="ID_428065283" MODIFIED="1403620211953" TEXT="object positioning">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1403619933991" ID="ID_1166607992" MODIFIED="1403620211957" TEXT="computer window detection">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1403619941690" ID="ID_1052817825" MODIFIED="1403620211960" TEXT="games for blind">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1403619758664" ID="ID_425541770" MODIFIED="1403620211968" POSITION="left" TEXT="Molecular Modeling">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1403619953052" ID="ID_1848153881" MODIFIED="1403620211975" TEXT="visualize molecules">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1403619964191" ID="ID_1284367904" MODIFIED="1403620211979" TEXT="feel intermolecular/atomic forces">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1403619981709" ID="ID_1521516839" MODIFIED="1403620211984" TEXT="gain understanding of activation energies">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1403627394581" ID="ID_1801541824" MODIFIED="1403627406894" TEXT="education">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1403627280114" ID="ID_114890001" MODIFIED="1403627286887" POSITION="left" TEXT="glove hood telemanipulation">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1403620087783" ID="ID_1474244607" MODIFIED="1403620211988" POSITION="right" TEXT="Other">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1403620101930" ID="ID_738233740" MODIFIED="1403620211995" TEXT="fine motor control with tight feedback">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1403620111090" ID="ID_265557160" MODIFIED="1403620212002" TEXT="ability to &quot;see&quot;/&quot;feel&quot; an extra dimension of data">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1403627132470" ID="ID_325418445" MODIFIED="1403627136974" POSITION="right" TEXT="physical therapy">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1403627156347" ID="ID_1974414191" MODIFIED="1403627168346" TEXT="buzzer when doing proper movement">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1403627169821" ID="ID_563769709" MODIFIED="1403627178952" TEXT="tracking over time to see if patients improve">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
